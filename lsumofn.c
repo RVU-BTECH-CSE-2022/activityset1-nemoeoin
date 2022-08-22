cfe8d8871736fc387a7f381c3ef73905d57f0d3b
teresa <110167230+teresavarghese@users.noreply.github.com>
2022-08-22 05:15:31 +0000

    sum of elements

---
+++ lsumofn.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int input (int *n)
+{
+  printf("enter the size of the array:");
+  scanf("%d",&n);
+}
+
+int inputarr(int arr[i])
+{
+  printf("Enter the elements in arrays");
+  for(int i=0;i<=n;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+}
+
+int compute(int sum, int arr[i])
+{
+  for(int i=0;i<=n;i++)
+    {
+      sum=sum+arr[i];
+    }
+  printf("The sum of the arrays %d",sum);
+  return 0;
+}
+
+void main()
+{
+  
+}
+
+
+int main()
+{
+  int n;
+  int arr[1000];
+  int sum=0;
+}
+  
+int input()
+{
+  printf("Enter the size of the array");
+    scanf("%d",&n);
+
+
+}
+
+int compute()
+{
+  for(int i=0;i<=n;i++)
+    {
+      sum=sum+arr[i];
+    }
+  printf("The sum of the arrays %d",sum);
+  return 0;
+}
+
+int output()
+{
+  
+}
